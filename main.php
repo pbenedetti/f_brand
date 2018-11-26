54e64766c635dd796de192cecad48378cbeb0b60
Paola <paola@MBP-di-FB.lan>
2018-11-26 11:53:51 +0100

    main.py

---
+++ main.php
@@ -0,0 +1,7 @@
+import numpy as np
+import nltk
+
+print "helloword"
+sentence = "At eight o'clock on Thursday morning... Arthur didn't feel very good."
+tokens = nltk.word_tokenize(sentence)
+print tokens
